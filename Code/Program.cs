f85ef969a007b2a88e4f352d9a82232070d1d7df
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-03-10 11:13:03 +0300

    Wrote code

---
+++ Code/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите элементы массива через пробел:");
+string input = Console.ReadLine();
+string[] stringsArray = input.Split(' ');
+
+List<string> result = new List<string>();
+
+for (int i = 0; i < stringsArray.Length; i ++)
+{
+    if (stringsArray[i].Length == 3)
+    {
+        result.Add(stringsArray[i]);
+    }
+}
+
+string[] resultArray = result.ToArray();
+Console.WriteLine($"[{string.Join(", ", resultArray)}]");
